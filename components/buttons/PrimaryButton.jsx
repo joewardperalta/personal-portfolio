43880e59e969038e3952883aab5807e9566ca9e5
Joeward Peralta <joeward_peralta@outlook.com>
2025-07-09 18:56:18 -0400

    Update the background color of the primary button to a subtle background color.

--- components/buttons/PrimaryButton.jsx
+++ components/buttons/PrimaryButton.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 export default function PrimaryButton({ children, className }) {
   return (
     <Button
-      className={`bg-secondary text-primary hover:bg-primary hover:text-secondary duration-300 ${className}`}
+      className={`bg-secondary text-primary hover:bg-secondary-dark duration-300 ${className}`}
     >
       {children}
     </Button>
