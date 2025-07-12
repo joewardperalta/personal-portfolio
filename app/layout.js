028aacc62451b5bab39085ba347b2c064db73144
Joeward Peralta <joeward_peralta@outlook.com>
2025-07-12 17:41:33 -0400

    Add a tertiary button class

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
 import { Geist } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Paragraph } from "@/components/Typography";
+import Section from "@/components/Section";
+import Wrapper from "@/components/Wrapper";
+import Link from "next/link";
+import Flex from "@/components/Flex";
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -18,6 +23,31 @@ export default function RootLayout({ children }) {
         <Navbar />
 
         {children}
+
+        <footer className="footer">
+          <Section>
+            <div className="py-2 mt-[60px] border-t border-tertiary-dark">
+              <Flex justifyContent="between">
+                <Paragraph>
+                  Designed by <span className="text-white">Joeward</span>
+                </Paragraph>
+                <ul className="text-white flex gap-4">
+                  <li>
+                    <Link className="tertiary-btn" href="/privacy-policy">
+                      Privacy Policy
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="tertiary-btn" href="/terms-and-conditions">
+                      Terms & Conditions
+                    </Link>
+                  </li>
+                </ul>
+                <Paragraph>&copy; 2025 All rights reserved.</Paragraph>
+              </Flex>
+            </div>
+          </Section>
+        </footer>
       </body>
     </html>
   );
